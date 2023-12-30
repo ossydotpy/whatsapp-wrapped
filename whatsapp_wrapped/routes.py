48d5991b701a4ddf712b96d36a9cb78a9c2268d5
ossydotpy <mrali.osman.6@gmail.com>
2023-12-30 01:30:08 +0000

    * Removed WordCloud * Completed 5/6 images

--- whatsapp_wrapped/routes.py
+++ whatsapp_wrapped/routes.py
@@ -1,4 +1,5 @@
-import io, base64
+import io, base64, json
+from .utils import edit_count_image, edit_top_initiator, edit_active_hours, edit_sentiment_analysis, edit_individual_count_image
 from flask import render_template, request, send_file, current_app
 from whatsapp_wrapped import app
 
@@ -12,18 +13,31 @@ def index():
             return render_template("index.html", error="No file part")
 
         response = current_app.test_client().post(
-            "/api/wordcloud/generate", data={"file": (file, "chat.txt")}
+            "/api/analytics/chat_analysis", data={"file": (file, "chat.txt")}
         )
 
         if response.status_code == 200:
-            img_data = response.data
-            img_base64 = base64.b64encode(img_data).decode("utf-8")
+            json_data = json.loads(response.data)
+            chat_count = edit_count_image(json_data)
+            top_initiator = edit_top_initiator(json_data)
+            peak_hours = edit_active_hours(json_data)
+            sentiments = edit_sentiment_analysis(json_data)
+            individual_stats = edit_individual_count_image(json_data)
 
-            return render_template("results.html", wordcloud_image=img_base64)
+            chat_count_base64 = base64.b64encode(chat_count.getvalue()).decode("ascii")
+            top_initiator_base64 = base64.b64encode(top_initiator.getvalue()).decode("ascii")
+            peak_hours_base64 = base64.b64encode(peak_hours.getvalue()).decode("ascii")
+            sentiments_base64 = base64.b64encode(sentiments.getvalue()).decode("ascii")
+            individual_stats_base64 = base64.b64encode(individual_stats.getvalue()).decode("ascii")
+
+            return render_template("results.html", chat_count_img=chat_count_base64,\
+                                   top_ini = top_initiator_base64, peak_img = peak_hours_base64,\
+                                    sentiments_img =sentiments_base64, individual_img=individual_stats_base64)
         else:
-            return render_template("index.html", error="Error generating Word Cloud")
+            return render_template("index.html", error="Error generating some images")
 
     return render_template("index.html")
+
 
 
 @app.route("/download_wordcloud")
--- whatsapp_wrapped/utils/helper_functions.py
+++ whatsapp_wrapped/utils/helper_functions.py
@@ -1,7 +1,10 @@
-import re
+from io import BytesIO
+import os, re
 import pandas as pd
 from datetime import datetime
 from nltk.sentiment import SentimentIntensityAnalyzer
+from PIL import Image, ImageDraw, ImageFont
+from flask import url_for
 
 sia = SentimentIntensityAnalyzer()
 
@@ -9,6 +12,16 @@ current_year = datetime.now().year
 new_year = f"1/1/{str(current_year)}"
 new_year_timestamp = pd.to_datetime(new_year, format="%m/%d/%Y").date()
 
+
+def get_fonts_dir():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(current_dir, '..', 'static', 'fonts')
+    return font_path
+
+def get_img_dir():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(current_dir, '..', 'static', 'images')
+    return font_path
 
 def preprocess_chat(chat_data):
     """Convert chat into a list of lists consisting of date, time, sender, message."""
@@ -117,3 +130,157 @@ def get_message_count_stats(df):
 
     sender_value_counts = df.groupby("Sender")["Message"].count()
     return sender_value_counts.to_dict()
+
+def edit_count_image(some_api_result: dict):
+    img_buffer = BytesIO()
+
+    image = Image.open(f'{get_img_dir()}/message_count.png')
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
+
+    bold_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Bold.ttf', 200)
+
+    message_count = str(some_api_result.get('message_count', None))
+    text_color = (255, 255, 255)
+    position = (0.31 * width, 0.12 * height)
+
+    draw.text(position, text=message_count, font=bold_font, fill=text_color)
+
+    image.save(img_buffer, format='PNG')
+    img_buffer.seek(0) 
+
+    return img_buffer
+
+
+def edit_top_initiator(some_api_result: dict):
+    img_buffer = BytesIO()
+
+    top_initiator = max(some_api_result['number_of_chat_initiations'],\
+                         key=lambda k: some_api_result['number_of_chat_initiations'][k])
+    value_counts = str(some_api_result['number_of_chat_initiations'][top_initiator])
+
+
+    image = Image.open(f'{get_img_dir()}/initiator.png')
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
+
+    text_color = (0, 0, 0) 
+    position1 = (0.20*width, 0.39*height)
+    position2 = (0.13*width, 0.32*height)
+
+    bold_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Bold.ttf', 100)
+
+    draw.text(position1, value_counts, font=bold_font, fill=text_color)
+    draw.text(position2, top_initiator, font=bold_font, fill=text_color)
+
+    image.save(img_buffer, format='PNG')
+    img_buffer.seek(0) 
+
+    return img_buffer
+
+
+def edit_active_hours(some_api_result):
+    text1 =some_api_result['peak_active_hours'].get('time_range', None)
+    text2 = some_api_result['peak_active_hours'].get('peak_time', None)
+    
+    img_buffer = BytesIO()
+
+    image = Image.open(f'{get_img_dir()}/active_hours.png')
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
+
+    text_color = (255, 255, 255) 
+    position1 = (0.14*width, 0.48*height)
+    position2 = (0.13*width, 0.32*height)
+
+    bold_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Bold.ttf', 100)
+
+    draw.text(position1, text1, font=bold_font, fill=text_color)
+    draw.text(position2, text2, font=bold_font, fill=text_color)
+
+    image.save(img_buffer, format='PNG')
+    img_buffer.seek(0) 
+
+    return img_buffer
+
+def edit_sentiment_analysis(some_api_result):
+    img_buffer = BytesIO()
+
+    image = Image.open(f'{get_img_dir()}/sentiment_analysis.png')
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
+
+    regular_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Regular.ttf', 30)
+    medium_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Medium.ttf', 30)
+    bottom_medium_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Medium.ttf', 40)
+
+    senders = list(some_api_result['sender_sentiment_percentage'].keys())
+
+    sender_sentiment_percentage = some_api_result['sender_sentiment_percentage']
+
+    most_positive_sender = None
+    max_positive_percentage = -1
+
+    for sender, sentiment_data in sender_sentiment_percentage.items():
+        positive_percentage = sentiment_data['positives']
+        
+        if positive_percentage > max_positive_percentage:
+            max_positive_percentage = positive_percentage
+            most_positive_sender = sender
+
+    format_sentiment = lambda sentiment: f"{round(sentiment, 2)} %"
+
+    for idx, sender in enumerate(senders, start=1):
+        sender_sentiment = some_api_result['sender_sentiment_percentage'].get(sender, {})
+
+        cell_a = (0.10*width, (0.39 + (idx-1)*0.05)*height)
+        cell_b = (0.40*width, (0.39 + (idx-1)*0.05)*height)
+        cell_c = (0.60*width, (0.39 + (idx-1)*0.05)*height)
+        cell_d = (0.80*width, (0.39 + (idx-1)*0.05)*height)
+
+        draw.text(cell_a, sender, font=medium_font, fill=(255, 255, 255))
+        draw.text(cell_b, format_sentiment(sender_sentiment.get('positives', 0)), font=regular_font, fill=(255, 255, 255))
+        draw.text(cell_c, format_sentiment(sender_sentiment.get('negatives', 0)), font=regular_font, fill=(255, 255, 255))
+        draw.text(cell_d, format_sentiment(sender_sentiment.get('neutrals', 0)), font=regular_font, fill=(255, 255, 255))
+
+    most_positive_sender_position = (0.38*width, 0.535*height)
+    draw.text(most_positive_sender_position, most_positive_sender, font=bottom_medium_font, fill=(255, 255, 255))
+
+    image.save(img_buffer, format='PNG')
+    img_buffer.seek(0)
+
+    return img_buffer
+
+def edit_individual_count_image(some_api_result: dict):
+    img_buffer = BytesIO()
+
+    image = Image.open(f'{get_img_dir()}/individual.png')
+    text_color = (0, 0, 0)
+    width, height = image.size
+    draw = ImageDraw.Draw(image)
+
+    bold_font = ImageFont.truetype(f'{get_fonts_dir()}/OpenSans-Bold.ttf', 150)
+
+    counts = some_api_result.get('message_count_by_sender', None)
+    senders = list(counts.keys())
+    
+    sender1 = senders[0]
+    sender2 = senders[1]
+
+    sender1_count = str(counts[sender1])
+    sender2_count = str(counts[sender2])
+
+    sender1_position = (0.02 * width, 0.58 * height)
+    sender2_position = (0.59 * width, 0.68 * height)
+    sender1_count_position = (0.05 * width, 0.67 * height)
+    sender2_count_position = (0.61 * width, 0.78 * height)
+
+    draw.text(sender1_position, text=sender1, font=bold_font, fill=text_color)
+    draw.text(sender2_position, text=sender2, font=bold_font, fill=text_color)
+    draw.text(sender1_count_position, text=sender1_count, font=bold_font, fill=text_color)
+    draw.text(sender2_count_position, text=sender2_count, font=bold_font, fill=text_color)
+
+    image.save(img_buffer, format='PNG')
+    img_buffer.seek(0) 
+
+    return img_buffer
